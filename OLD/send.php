2d66c3b981186d3af401ea6561dd31494b750bcf
Bruno Rocha Agostinho <brurago@gmail.com>
2017-11-08 21:33:02 -0200

    Landing OLD

---
+++ OLD/send.php
@@ -0,0 +1,25 @@
+
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+ <meta name="description" content="Domínio DEUS.com.vc à venda para igrejas, templos, empresas, religiosos e afins" />
+ <meta name="keywords" content="deus,igreja,religião,bíblia,fé,padre,irmão,crente,pastor,sacerdote" />
+
+<title>Deus.com.vc - Domínio DEUS.com.vc à venda para igrejas, templos, empresas, religiosos e afins</title>
+
+<link rel="stylesheet" type="text/css" href="bootstrap.css">
+<link rel="stylesheet" type="text/css" href="styles.css">
+</head>
+
+<body>
+<div class="container">  
+  <div class="box-soon">
+    <h1>Contato enviado com sucesso! </h1><br><br>
+    <h3>Entraremos em contato</h3>
+ 
+
+</div>
+<div class="shape-logo"> <img src="images/magnadev-logo.png" width="191" height="85" alt="Logotipo Magnadev" /> </div>
+</body>
+</html>
